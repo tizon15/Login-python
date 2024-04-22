9c4186058565f451e8a817ba2b39233e49869ed1
Álvaro Martínez-Tizón García <amartineztizon@gmail.com>
2024-04-22 20:18:42 +0200

    adding the while loop to finish when the user clicks exit and split in functions the code

--- db.py
+++ db.py
@@ -2,41 +2,40 @@ from replit import db
 
 
 def get_list_of_users():
-    """return a list of users"""
-    return print(f"Usuarios: {list(db.keys())}")
+  return print(f"Usuarios: {list(db.keys())}\n")
+
+
+def get_user(username):
+  return db.get(username)
 
 
 def add_user(user):
-    """add a user to the list of users"""
-    db[user.get_username()] = {
-        'username': user.get_username(),
-        'password': user.get_password(),
-        'role': user.get_role()
-    }
+  db[user.get_username()] = {
+      'username': user.get_username(),
+      'password': user.get_password(),
+      'role': user.get_role()
+  }
 
 
 def validate(username, password):
-    """checks if we have a username and password correct"""
-    user = db.get(username)
-    if user and user['password'] == password:
-        return True
-    return False
+  user = db[username]
+  print(user)
+  if user and user['password'] == password:
+    return True
+  return False
 
 
-def edit_user(user, new_user):
-    """checks if the user is already in the list of users and edits it with the new user"""
-    if user in db.keys() and user == new_user.get_username():
-        delete_user(user)
-        add_user(new_user)
-        print(f"User {db[user.get_username()]['username']} update.")
-    else:
-        print('el usuario no esta en la base de datos')
+def edit_user(username, new_user):
+  if username in db:
+    delete_user(username)
+    add_user(new_user)
+    print(f"User {db[new_user.get_username()]['username']} update.")
+  else:
+    print('el usuario no esta en la base de datos')
 
 
 def delete_user(username):
-    """Deletes a user from the database"""
-    try:
-        del db[username]
-        print(f"User {username} deleted.")
-    except:
-        print("the user is not in database")
+  try:
+    del db[username]
+  except:
+    print("the user is not in database")
--- login.py
+++ login.py
@@ -1,72 +1,99 @@
 import getpass
-from user import User
 import db
+from user import User
 
 
 def login():
-    """ function to start the process of login"""
-    print("Bienvenido al sistema de registro de usuarios")
-    if input("¿Tiene usuario? (s/n)").lower() == "s":
-        usuario = input("Ingrese el nombre de usuario: ")
-        contraseña = getpass.getpass(prompt="Ingrese la contraseña: ")
-        if db.validate(usuario, contraseña):
-            print("¡Bienvenido! ¿Qué desea hacer?")
-            match int(
-                input(
-                    '1. Agregar usuario\n2. Editar usuario\n3. Eliminar usuario\n4. Ver usuarios\n5. Salir\n'
-                )):
-                case 1:
-                    agregar_usuario()
-                case 2:
-                    print("que valor quieres editar")
-                    editar_usuario(usuario, contraseña)
-                case 3:
-                    username = input("Ingrese el nombre de usuario a eliminar: ")
-                    db.delete_user(username)
-                case 4:
-                    db.get_list_of_users()
-                case _:
-                    print("Hasta pronto")
-        else:
-            print("Usuario o contraseña incorrectos")
+  user = User("", "", "")
+  # Pedir al usuario que ingrese los datos del nuevo usuario
+  print("Bienvenido al sistema de registro de usuarios")
+  if (input("¿Tiene usuario? (s/n): ").lower() == "s"):
+    usuario = input("Ingrese el nombre de usuario: ")
+    contraseña = getpass.getpass(prompt="Ingrese la contraseña: ")
+    if (db.validate(usuario, contraseña)):
+      user.set_username(db.get_user(usuario)['username'])
+      user.set_password(db.get_user(usuario)['password'])
+      user.set_role(db.get_user(usuario)['role'])
+      print("¡Bienvenido! ¿Qué desea hacer?")
+      display_menu(user)
     else:
-        if input("¿Desea registrarse? (s/n)").lower() == "s":
-            agregar_usuario()
-        else:
-            print("Hasta pronto")
+      print("Usuario o contraseña incorrectos")
+  else:
+    if (input("¿Desea registrarse? (s/n): ").lower() == "s"):
+      agregar_usuario()
+      display_menu(user)
+    else:
+      print("Hasta pronto")
+
+
+def display_menu(user):
+  while True:
+    match int(
+        input(
+            "1. Agregar usuario\n2. Editar usuario\n3. Eliminar usuario\n4. Ver usuarios\n5. Salir\n"
+        )):
+      case 1:
+        agregar_usuario()
+      case 2:
+        print("que valor quieres editar")
+        editar_usuario()
+      case 3:
+        username = input("Ingrese el nombre de usuario a eliminar: ")
+        db.delete_user(username)
+        print(f"User {username} deleted.")
+      case 4:
+        db.get_list_of_users()
+      case 5:
+        print("Hasta pronto")
+        break
+      case _:
+        print('Opcion inválida')
 
 
 def agregar_usuario():
-    """create the inputs of the consoles and with the retrun create the new user"""
-    # Creamos los inputs en una funcion para que quede mas limpio el codigo
-    nombre = input("Ingrese el nombre del usuario: ")
-    password = getpass.getpass(prompt="Ingrese la contraseña: ")
-    rol = input("Ingrese el rol del usuario: ")
-    user = User(nombre, password, rol)
-    db.add_user(user)
-    print(f"User {user.get_username()} added to the database.")
+  # Creamos los inputs en una funcion para que quede mas limpio el codigo
+  new_user = User("", "", "")
+  new_user.set_username(input("Ingrese el nombre del usuario: "))
+  new_user.set_password(getpass.getpass(prompt="Ingrese la contraseña: "))
+  new_user.set_role(input("Ingrese el rol del usuario: "))
+  db.add_user(new_user)
+  print(f"Usuario {new_user.get_username()} añadido.\n")
+  print("¿Quieres gestionar algo más?\n")
 
-def editar_usuario(usuario, password):
-    """"We ask the user which parameter wants to edit"""
-    match int(
-        input(
-            "1. nombre usuario\n2. cambiar contraseña\n3. cambiar rol\n")
-    ):
-        case "1":
-            new_username = input("Ingrese el nuevo de usuario: ")
-            new_user = User(new_username, password,
-                            db.db.get(usuario)['role'])
-            db.edit_user(usuario, new_user)
-        case "2":
-            new_password = input("Ingrese la nueva contraseña: ")
-            new_user = User(usuario, new_password,
-                            db.db.get(usuario)['role'])
-            db.edit_user(usuario, new_user)
-        case "3":
-            new_role = input("Ingrese el nuevo rol: ")
-            new_user = User(usuario, password, new_role)
-            db.edit_user(usuario, new_user)
+
+def editar_usuario():
+  match int(
+      input("1. nombre usuario\n2. cambiar contraseña\n3. cambiar rol\n")):
+    case 1:
+      username = input("Ingrese el nombre del usuario que quiere editar: ")
+      get_user = db.get_user(username)
+      new_user = User(get_user['username'], get_user['password'],
+                      get_user['role'])
+      new_user.set_username(input("Ingrese el nuevo nombre de usuario: "))
+      db.edit_user(username, new_user)
+    case 2:
+      username = input("Ingrese el nombre del usuario que quiere editar: ")
+      get_user = db.get_user(username)
+      new_user = User(get_user['username'], get_user['password'],
+                      get_user['role'])
+      new_user.set_username(
+          getpass.getpass(prompt="Ingrese la nueva contraseña: "))
+      db.edit_user(username, new_user)
+    case 3:
+      username = input("Ingrese el nombre del usuario que quiere editar: ")
+      get_user = db.get_user(username)
+      new_user = User(get_user['username'], get_user['password'],
+                      get_user['role'])
+      new_user.set_role(input("Ingrese el nuevo rol: "))
+
+      db.edit_user(username, new_user)
+
+
+def main():
+  """Entry point for the application."""
+  login()
+
 
 # Ejemplo de uso en la terminal
 if __name__ == "__main__":
-    login()
+  main()
